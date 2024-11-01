b7c653ff008f2f1bde68cfc8f4fedd784c8e0ccf
Tolga-dev <orhan.konat.12@gmail.com>
2024-11-02 00:25:54 +0300

    Bit map

--- Assets/Scripts/BitMapGenerator/BitMapWorldGenerator.cs
+++ Assets/Scripts/BitMapGenerator/BitMapWorldGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,21 +9,27 @@ namespace BitMapGenerator
     public class BitMapWorldGenerator : MonoBehaviour
     {
         public Transform generationStartPoint;
-        public Vector2Int sizeOfGridWorld;
         public float offset;
-
+        public GameObject houseObject;
+        
+        public GameObject tripleRoadObject;
+        public GameObject forthRoadObject;
+        public GameObject twoRoadObject;
+        public GameObject cornerRoadObject;
+        
+        public Vector2Int sizeOfGridWorld;
         public List<BuildCells> buildCells = new List<BuildCells>();
 
-        private const int HOUSE_CODE = 1;
-        private const int EMPTY_CODE = 0;
-        private const int ROAD_CODE = 2;
+        private const int HOUSE_CODE = 0;
+        private const int ROAD_CODE = 1;
 
         private void Start()
-        {
+        {   
             GenerateWorld();
             PrintWorldGrid();
+            VisualizeWorld();
         }
-
+        
         private void GenerateWorld()
         {
             for (int x = 0; x < sizeOfGridWorld.x; x++)
@@ -44,25 +51,27 @@ namespace BitMapGenerator
         {
             var buildCellsInRow = new List<BuildCell>();
 
-            bool isHouseRow = (x % 3 == 0);
+            bool isRoadRow = (x % 3 == 0);
             for (int y = 0; y < sizeOfGridWorld.y; y++)
             {
-                int code = DetermineCellCode(isHouseRow, y);
+                int code = DetermineCellCode(isRoadRow, y);
                 buildCellsInRow.Add(new BuildCell { code = code });
             }
 
             return buildCellsInRow;
         }
 
-        private int DetermineCellCode(bool isHouseRow, int y)
+        private int DetermineCellCode(bool isRoad, int y)
         {
-            if (isHouseRow || y == 0 || y == sizeOfGridWorld.y - 1)
+            if (isRoad || y == 0 || y == sizeOfGridWorld.y - 1)
             {
-                return HOUSE_CODE;
+                return ROAD_CODE;
             }
 
-            return EMPTY_CODE;
+            return HOUSE_CODE;
         }
+        
+        
 
         private void PlaceRoadsInRow(int rowIndex)
         {
@@ -72,6 +81,9 @@ namespace BitMapGenerator
 
             for (int i = 0; i < roadCount; i++)
             {
+                if(availableIndexes.Count == 0)
+                    break;
+                
                 RemoveHouseForRoad(availableIndexes, rowIndex);
             }
         }
@@ -89,7 +101,7 @@ namespace BitMapGenerator
 
         private void RemoveHouseForRoad(List<int> availableIndexes, int rowIndex)
         {
-            int randomIndex = availableIndexes[Random.Range(0, availableIndexes.Count)];
+            int randomIndex = availableIndexes[Random.Range(0, availableIndexes.Count-1)];
 
             buildCells[rowIndex].cells[randomIndex].code = ROAD_CODE;
             buildCells[rowIndex + 1].cells[randomIndex].code = ROAD_CODE;
@@ -100,14 +112,95 @@ namespace BitMapGenerator
         private void RemoveNearbyIndexes(List<int> availableIndexes, int index)
         {
             availableIndexes.Remove(index);
-            availableIndexes.Remove(index + 1);
-            availableIndexes.Remove(index - 1);
+            availableIndexes.Remove(index+1);
+            availableIndexes.Remove(index-1);
         }
 
         private int CalculateMaxReplacements(int bitCount)
         {
             return (bitCount % 2 == 0) ? (bitCount / 2) : (bitCount / 2 + 1);
         }
+        
+        private void VisualizeWorld()
+        {
+            for (int i = 0; i < buildCells.Count; i++)
+            {
+                for (int j = 0; j < buildCells[i].cells.Count; j++)
+                {
+                    var code = buildCells[i].cells[j].code;
+                    Vector3 spawnPosition = generationStartPoint.position + new Vector3(i * offset, 0, j * offset);
+   
+                    if (code == 0)
+                    {
+                        var house = Instantiate(houseObject, spawnPosition, Quaternion.identity);
+                    }
+                    else
+                    {
+
+                        bool hasLeftNeighbor = (j > 0) && (buildCells[i].cells[j - 1].code == ROAD_CODE);
+                        bool hasRightNeighbor = (j < buildCells[i].cells.Count - 1) && (buildCells[i].cells[j + 1].code == ROAD_CODE);
+                        bool hasTopNeighbor = (i > 0) && (buildCells[i - 1].cells[j].code == ROAD_CODE);
+                        bool hasBottomNeighbor = (i < buildCells.Count - 1) && (buildCells[i + 1].cells[j].code == ROAD_CODE);
+
+                        int roadNeighbors = (hasLeftNeighbor ? 1 : 0) + (hasRightNeighbor ? 1 : 0) + (hasTopNeighbor ? 1 : 0) + (hasBottomNeighbor ? 1 : 0);
+
+                        if (roadNeighbors == 3)
+                        {
+                            var road = Instantiate(tripleRoadObject, spawnPosition, Quaternion.identity);
+
+                            if (!hasLeftNeighbor) road.transform.rotation = Quaternion.Euler(0, 270, 0);      // "T" shape facing right
+                            else if (!hasRightNeighbor) road.transform.rotation = Quaternion.Euler(0, 90, 0); // "T" shape facing left
+                            else if (!hasTopNeighbor) road.transform.rotation = Quaternion.Euler(0, 0, 0);   // "T" shape facing down
+                            else if (!hasBottomNeighbor) road.transform.rotation = Quaternion.Euler(0, 180, 0); // "T" shape facing up
+                        }
+                        else if (roadNeighbors == 4)
+                        {
+                            var road = Instantiate(forthRoadObject, spawnPosition, Quaternion.identity);
+                            road.transform.rotation = Quaternion.identity; // Cross, no rotation needed
+                        }
+                        else
+                        {
+                            
+                            if (i == 0 && j == 0) // lu
+                            {
+                                var road = Instantiate(cornerRoadObject, spawnPosition, Quaternion.identity);
+                                road.transform.rotation = Quaternion.Euler(0, 90, 0); // Adjust rotation as needed
+                            }
+                            else if (i == 0 && j == buildCells[i].cells.Count - 1) // ru
+                            {
+                                var road = Instantiate(cornerRoadObject, spawnPosition, Quaternion.identity);
+                                road.transform.rotation = Quaternion.Euler(0, 180, 0); // Adjust rotation as needed
+                            }
+                            else if (i == buildCells.Count - 1 && j == 0) // ld
+                            {
+                                var road = Instantiate(cornerRoadObject, spawnPosition, Quaternion.identity);
+                                road.transform.rotation = Quaternion.Euler(0, 0, 0); // Adjust rotation as needed
+                            }
+                            else if (i == buildCells.Count - 1 && j == buildCells[i].cells.Count - 1) // rd
+                            {
+                                var road = Instantiate(cornerRoadObject, spawnPosition, Quaternion.identity);
+                                road.transform.rotation = Quaternion.Euler(0, 270, 0); // Adjust rotation as needed
+                            }
+                            else
+                            {
+                                var road = Instantiate(twoRoadObject, spawnPosition, Quaternion.identity);
+                                bool isRoadRow = (i % 3 == 0);
+                                if (isRoadRow && (j != 0 || j != buildCells[i].cells.Count - 1))
+                                {
+                                    road.transform.rotation = Quaternion.Euler(0,90,0);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+
+
+       
+            
+        }
+
 
         private void PrintWorldGrid()
         {
@@ -123,7 +216,7 @@ namespace BitMapGenerator
             }
 
             Debug.Log(gridOutput.ToString());
-        }s
+        }
     }
 
     [Serializable]
